1ab8c7061bd6ad25609339fab1d6ae3d224053a8
twesigetrejan <trejantwesige19@gmail.com>
2025-06-11 09:47:49 +0300

    Day2: adding custom modules

---
+++ dummy/__manifest__.py
@@ -0,0 +1,17 @@
+# {
+#     "name": "dummy",
+#     "installable": true,
+#     "application": true,
+#     "category": "Tools",
+#     "version": "1.0",
+#     "summary": "A dummy module to test custom addons"
+# }
+
+{
+    'name': 'dummy',
+    'installable': True,
+    'application': True,
+    'category': 'Tools',
+    'version': '1.0',
+    'summary': 'A dummy module to test custom addons',
+}
